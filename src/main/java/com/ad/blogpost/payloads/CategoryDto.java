69e54b37e908639d64961f8f6cb4f02e147d0336
ADlv20 <adwaitg02@gmail.com>
2022-10-25 19:42:29 +0530

    Validations

--- src/main/java/com/ad/blogpost/payloads/CategoryDto.java
+++ src/main/java/com/ad/blogpost/payloads/CategoryDto.java
@@ -4,13 +4,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class CategoryDto {
 
     private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 20, message = "Must be between 2-20 characters")
     private String categoryTitle;
+
     private String categoryDescription;
 
 }
