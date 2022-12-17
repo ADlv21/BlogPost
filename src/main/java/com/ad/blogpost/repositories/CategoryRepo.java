612b0e0e9aca323898dede6081e5023d933b503f
ADlv20 <adwaitg02@gmail.com>
2022-12-17 11:21:50 +0530

    Naming Changes

--- src/main/java/com/ad/blogpost/repositories/CategoryRepo.java
+++ src/main/java/com/ad/blogpost/repositories/CategoryRepo.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CategoryRepo extends JpaRepository<Category, Long> {
 
     // Gets all the info of a specific category
-    Category findByTitle(String categoryName);
+    Category findByTitle(String categoryTitle);
 
 }
